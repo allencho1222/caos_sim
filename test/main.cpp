62563e8209e0b98d10ed1c9333eb8815d17a23f5
sungjun <allencho1222@gmail.com>
2023-05-02 21:51:04 +0900

    enable test code to load package srcs

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,12 @@
 #include <fmt/format.h>
 #include <gtest/gtest.h>
 
+#include "nand_flash_memory/Package.h"
+
 int main(int argc, char** argv) {
   fmt::print("HELLO test {} {}\n", 1, 3);
   ::testing::InitGoogleTest(&argc, argv);
+  // For testing compilation
+  auto flashMem = std::make_unique<FlashMemory>(4, 4, 4, 4, 4);
   return 0;
 }
